7119c1a9ecd21a9facf3dea93a024cac73f96c6d
rengifolu <andrelujan7@gmail.com>
2020-03-07 14:32:38 +0100

    configuracion de base de datos y modelos crud - creo que falta correcto uri y pass a db

---
+++ models/Expense.js
@@ -0,0 +1,12 @@
+//models/Expense.js
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var expenseSchema = new Schema({
+  description: String,
+  amount: Number,
+  month: String,
+  year: Number
+});
+
+module.exports = mongoose.model('Expense', expenseSchema);
